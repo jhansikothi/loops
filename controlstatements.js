fba04da5ff5dce7098aceed318b34d5788b26e0d
jhansi kothi <jhansijanu2172@gmail.com>
2024-10-25 09:37:19 +0530

    Add files via upload

---
+++ controlstatements.js
@@ -0,0 +1,67 @@
+/*console the numbers from 1 to 2*/
+for(var a=1;a<3;a++){
+    console.log(a)
+}
+/*console the numbers from 1 greater than or equal to 5*/
+for(var a=1;a<=5;a++){
+    console.log(a)
+}
+/*console the numbers from 7 greater than or equal to 12*/
+for(var a=7;a<=12;a++){
+    console.log(a)
+}
+/*console the numbers 11,10,9,8,7 by using for loop*/
+for(var a=11;a>6;a--){
+    console.log(a)
+}
+/*console the numbers 24,23,22,21,20 so on 15 by using for loop*/
+for(var a=24;a>=15;a--){
+    console.log(a)
+}
+/*console the numbers 12,11,10,9,8 by using for loop*/
+for(var a=12;a>7;a--){
+    console.log(a)
+}
+/*console the numbers 1 to 7 by using while loop*/
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++
+}
+/*console the numbers 10,9,8,7,6 by using while loop*/
+var a=10;
+while(a>=6){
+    console.log(a)
+    a--
+}
+/* print the output if console is outside the block by using for loop*/
+for(var i=0;i<=5;i++){
+
+}
+console.log(i)
+/*print the output by using while loop */
+var i=1;
+while(i<4){
+    i++
+}
+console.log(i)
+/*console the numbers 2,3,4,5,6 by using dowhile */
+var i=1;
+do{
+    i++
+    console.log(i)
+}while(i<=5)
+
+/*console the numbers 9,8,7,6 by using dowhile */
+    var a=10;
+    do{
+        a--
+        console.log(a)
+    }while(a>=7)
+/*console the numbers 10,9,8,7 by using dowhile */
+        var a=10;
+        do{
+            console.log(a)
+            a--
+        }while(a>=7)
+            
